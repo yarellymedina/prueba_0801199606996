b23191e1dddfcdaa551711f0b272ac567f9600c4
Unknown <azzmedina5@gmail.com>
2017-05-29 22:52:49 -0600

    Prueba Seminario Software

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Prueba Seminario Software' });
 });
+
+router.get('/signup', function(req,res,next){
+  res.render('vista_mensaje',{"txtMsg":"","msg":""});
+});
+
+var msgsSubmit = [];
+
+router.post('/signup',function(req,res,next){
+  console.log(req.body);
+    msgsSubmit.push("Prueba"+ " " + req.body.txtMsg);
+    var msgs = msgsSubmit;
+    var rtObject = {};
+    rtObject.txtMsg=req.body.txtMsg;
+      rtObject.msg=msgs;
+  res.render('vista_mensaje', rtObject);
+});
+
+router.get('/api/usuarios',function(req,res,next){
+  var usuarios=[
+    {"nombre":"Yarelly","apellido":"Medina"},
+    {"telefono":"33180229","coreeo":"azzmedina5@gmail.com"},
+    {"facultad":["Ingenieria en Ciencias de la Computación","4to año"]},
+    
+  ];
+  res.json(usuarios);
+});
+
 
 module.exports = router;
